f2ef8adeb61aad89a1142537877bdae02471cae7
Sanchit <132829514+Sanchitpatil08@users.noreply.github.com>
2025-01-04 22:39:21 +0530

    Update temp2.js

--- temp2.js
+++ temp2.js
@@ -1,3 +1,8 @@
 // code editied by sanchit into forked repository
 
 console.log("Hello World from the temp2 .js file")
+
+
+// commeit 2 in the pr 2
+
+console.log("Hello World from the temp2 .js file")
